bc9cc06e30be43ccc55f10019ff5c37a61b0937e
Alvaro17_max <aavalos@fi.uba.ar>
2026-09-20 16:01:03 -0300

    Implemento lógica para listar socios con paginación y filtro

--- src/routes/socios_routes.py
+++ src/routes/socios_routes.py
@@ -1,9 +1,28 @@
+from flask import jsonify, request
+from src.services.socios_services import (
+    obtener_todos_los_socios,
+    buscar_socio_por_id,
+    registrar_nuevo_socio,
+    modificar_socio,
+    existe_socio_con_email,
+)
+
 def register_socios_routes(app):
     @app.route("/socios", methods=["GET"])
     def get_socios():
         # Listar socios con paginación. Filtros opcionales: nombre y activo.
         # Página 6
-        pass
+        nombre = request.args.get('nombre')
+        activo = request.args.get('activo')
+        pagina = request.args.get('pagina', default=1, type=int)
+        
+        activo_bool = None
+        if activo is not None:
+            activo_bool = activo.lower() == 'true'
+
+        socios = obtener_todos_los_socios(nombre=nombre, activo=activo_bool, pagina=pagina)
+        return jsonify(socios), 200
+
 
     @app.route("/socios", methods=["POST"])
     def create_socio():
